391e95576c9a40d9745471e71263106350565eae
mauroac26 <mauroac26@gmail.com>
2025-07-12 12:22:57 -0300

    Creacion views del carrito

--- backend/apps/cart/models.py
+++ backend/apps/cart/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from apps.product.models import Product
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
-# Create your models here.
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    total_items = models.IntegerField(default=0)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    count = models.IntegerField()
